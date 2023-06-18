c5b5ded48ecdeda3d19cdfab886c27eb8d20bea4
Hasib305 <hasibhasan305@gmail.com>
2023-06-18 21:47:13 +0600

    worked done

--- app/src/main/java/com/example/cardiacrecord/MainActivity.java
+++ app/src/main/java/com/example/cardiacrecord/MainActivity.java
@@ -1,16 +1,34 @@
 package com.example.cardiacrecord;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -24,11 +42,148 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private ProgressBar loadingPB;
     private ArrayList<UserRVModal> userRVModalArrayList;
-    private CourseRVAdapter courseRVAdapter;
-    private RelativeLayout homeRL;
+    private UserRVAdapter userRVAdapter;
+    private RelativeLayout bottom_sheetRl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // initializing all our variables.
+        userRV = findViewById(R.id.idRVUser);
+        bottom_sheetRl = findViewById(R.id.idRLHome);
+        loadingPB = findViewById(R.id.idPBLoading);
+        addUserFAB = findViewById(R.id.idFABAddUser);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        userRVModalArrayList = new ArrayList<>();
+        // on below line we are getting database reference.
+        databaseReference = firebaseDatabase.getReference("Users");
+        // on below line adding a click listener for our floating action button.
+        addUserFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // opening a new activity for adding a user.
+                Intent i = new Intent(MainActivity.this, AddUserActivity.class);
+                startActivity(i);
+            }
+        });
+        // on below line initializing our adapter class.
+        userRVAdapter = new UserRVAdapter(userRVModalArrayList, this, this::onUserClick);
+        // setting layout malinger to recycler view on below line.
+        userRV.setLayoutManager(new LinearLayoutManager(this));
+        // setting adapter to recycler view on below line.
+        userRV.setAdapter(userRVAdapter);
+        // on below line calling a method to fetch users from database.
+        getUsers();
+    }
+
+    private void getUsers() {
+        // on below line clearing our list.
+        userRVModalArrayList.clear();
+        // on below line we are calling add child event listener method to read the data.
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                // on below line we are hiding our progress bar.
+                loadingPB.setVisibility(View.GONE);
+                // adding snapshot to our array list on below line.
+                userRVModalArrayList.add(snapshot.getValue(UserRVModal.class));
+                // notifying our adapter that data has changed.
+                userRVAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                // this method is called when new child is added
+                // we are notifying our adapter and making progress bar
+                // visibility as gone.
+                loadingPB.setVisibility(View.GONE);
+                userRVAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                // notifying our adapter when child is removed.
+                userRVAdapter.notifyDataSetChanged();
+                loadingPB.setVisibility(View.GONE);
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                // notifying our adapter when child is moved.
+                userRVAdapter.notifyDataSetChanged();
+                loadingPB.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+   // @Override
+    public void onUserClick(int position) {
+        // calling a method to display a bottom sheet on below line.
+        displayBottomSheet(userRVModalArrayList.get(position));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // adding a click listener for option selected on below line.
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.idLogOut:
+                // displaying a toast message on user logged out inside on click.
+                Toast.makeText(getApplicationContext(), "User Logged Out", Toast.LENGTH_LONG).show();
+                // on below line we are signing out our user.
+                mAuth.signOut();
+                // on below line we are opening our login activity.
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // on below line we are inflating our menu
+        // file for displaying our menu options.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    private void displayBottomSheet(UserRVModal modal) {
+        // on below line we are creating our bottom sheet dialog.
+        final BottomSheetDialog bottomSheetTeachersDialog = new BottomSheetDialog(this);
+        // on below line we are inflating our layout file for our bottom sheet.
+        View layout = LayoutInflater.from(this).inflate(R.layout.bottom_sheet_dialog, bottom_sheetRl);
+        // setting content view for bottom sheet on below line.
+        bottomSheetTeachersDialog.setContentView(layout);
+        // on below line we are setting a cancelable
+        bottomSheetTeachersDialog.setCancelable(false);
+        bottomSheetTeachersDialog.setCanceledOnTouchOutside(true);
+        // calling a method to display our bottom sheet.
+        bottomSheetTeachersDialog.show();
+
+
+        Button editBtn = layout.findViewById(R.id.idBtnUpdate);
+
+        // adding on click listener for our edit button.
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // on below line we are opening our EditUserActivity on below line.
+                Intent i = new Intent(MainActivity.this, EditUserActivity.class);
+                // on below line we are passing our user modal
+                i.putExtra("user", modal);
+                startActivity(i);
+            }
+        });
+
     }
 }
